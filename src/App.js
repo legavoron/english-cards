0a991713f91fa4c43f9398e600048382c76195c4
Aleh Shchurko <alehshchurko@gmail.com>
2022-07-09 21:21:57 +0300

    create startPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+import Screensaver from './components/screensaver/screensaver';
+import StartPage from './components/startPage/startPage';
 function App() {
   return (
     <div className="App">
+      {/* <Screensaver/> */}
       
+      <StartPage/>
     </div>
   );
 }
